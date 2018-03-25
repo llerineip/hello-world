30cd06fbe697dc5464303ed6b3280b53405eb4b0
llerineip <37762584+llerineip@users.noreply.github.com>
2018-03-24 21:27:57 -0700

    Add files via upload
    Initial checkin

---
+++ HelloWorld.c
@@ -0,0 +1,13 @@
+/**
+ * @brief A hello world C program for GitHub testing
+ * 
+ * @file HelloWorld.c
+ * @author llerineip
+ * @date 2018-03-24
+ */
+
+#include <stdio.h>
+int main(int argc, char *argv[]){
+    printf("Hello World, C!\n");
+    return 0;
+}
